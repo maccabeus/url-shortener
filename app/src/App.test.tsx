7f1a504974280ec5a8a23d8fdc236ea328158f3f
Anthony Adeniyi <mlisoftinc@gmail.com>
2022-09-29 15:01:56 +0100

    refactor: update app test

--- app/src/App.test.tsx
+++ app/src/App.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders app correctly with link button', () => {
   render(<App />);
   const linkElement = screen.getByText(/shorten now/i);
   expect(linkElement).toBeInTheDocument();
